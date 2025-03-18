7d528ac885b2dd5faa5f0394ba4fcb860a077dc3
Riley <riley.mathieu@ravenind.com>
2025-03-18 07:56:02 -0400

    Apply cargo format

--- src/isoname.rs
+++ src/isoname.rs
@@ -1,275 +1,272 @@
 #[repr(transparent)]
 
-pub struct IsoName{
+pub struct IsoName {
     raw_value: u64,
 }
 
-impl IsoName{
+impl IsoName {
     pub fn new(value: u64) -> Self {
-        return Self{raw_value: value};
+        return Self { raw_value: value };
     }
-    pub fn get_address_arb_capable(&self) -> u8
-    {
+    pub fn get_address_arb_capable(&self) -> u8 {
         return (self.raw_value >> 63) as u8;
     }
 
-    pub fn set_address_arb_capable(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 63) & 0x8000000000000000; 
+    pub fn set_address_arb_capable(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 63) & 0x8000000000000000;
     }
 
-    pub fn get_industry_group(&self) -> u8
-    {
+    pub fn get_industry_group(&self) -> u8 {
         return ((self.raw_value >> 60) & 0x07) as u8;
     }
 
-    pub fn set_industry_group(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 60) & 0x7000000000000000; 
+    pub fn set_industry_group(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 60) & 0x7000000000000000;
     }
 
-    pub fn get_vehicle_system_instance(&self) -> u8
-    {
+    pub fn get_vehicle_system_instance(&self) -> u8 {
         return ((self.raw_value >> 56) & 0x0F) as u8;
     }
 
-    pub fn set_vehicle_system_instance(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 56) & 0x0F00000000000000; 
+    pub fn set_vehicle_system_instance(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 56) & 0x0F00000000000000;
     }
 
-    pub fn get_vehicle_system(&self) -> u8
-    {
+    pub fn get_vehicle_system(&self) -> u8 {
         return ((self.raw_value >> 49) & 0x7F) as u8;
     }
 
-    pub fn set_vehicle_system(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 49) & 0x00FE000000000000; 
+    pub fn set_vehicle_system(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 49) & 0x00FE000000000000;
     }
-    
-    pub fn get_function_code(&self) -> u8
-    {
+
+    pub fn get_function_code(&self) -> u8 {
         return ((self.raw_value >> 40) & 0xFF) as u8;
     }
 
-    pub fn set_function_code(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 40) & 0x0000FF0000000000; 
+    pub fn set_function_code(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 40) & 0x0000FF0000000000;
     }
 
-    pub fn get_function_code_instance(&self) -> u8
-    {
+    pub fn get_function_code_instance(&self) -> u8 {
         return ((self.raw_value >> 35) & 0x1F) as u8;
     }
 
-    pub fn set_function_code_instance(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 35) & 0x000000F800000000; 
+    pub fn set_function_code_instance(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 35) & 0x000000F800000000;
     }
 
-    pub fn get_ecu_instance(&self) -> u8
-    {
+    pub fn get_ecu_instance(&self) -> u8 {
         return ((self.raw_value >> 32) & 0x07) as u8;
     }
 
-    pub fn set_ecu_instance(&mut self, value: u8)
-    {
-        self.raw_value |= ((value as u64) << 32) & 0x0000000700000000; 
+    pub fn set_ecu_instance(&mut self, value: u8) {
+        self.raw_value |= ((value as u64) << 32) & 0x0000000700000000;
     }
 
-    pub fn get_mc_code(&self) -> u16
-    {
+    pub fn get_mc_code(&self) -> u16 {
         return ((self.raw_value >> 21) & 0x7FF) as u16;
     }
 
-    pub fn set_mc_code(&mut self, value: u16)
-    {
-        self.raw_value |= ((value as u64) << 21) & 0x00000000FFE00000; 
+    pub fn set_mc_code(&mut self, value: u16) {
+        self.raw_value |= ((value as u64) << 21) & 0x00000000FFE00000;
     }
 
-    pub fn get_serial(&self) -> u32
-    {
+    pub fn get_serial(&self) -> u32 {
         return (self.raw_value & 0x1FFFFF) as u32;
     }
 
-    pub fn set_serial(&mut self, value: u32)
-    {
-        self.raw_value |= (value as u64) & 0x00000000001FFFF; 
+    pub fn set_serial(&mut self, value: u32) {
+        self.raw_value |= (value as u64) & 0x00000000001FFFF;
     }
 
-    pub fn get_raw_value(&self) -> u64
-    {
+    pub fn get_raw_value(&self) -> u64 {
         return self.raw_value;
     }
-
 }
 
 #[cfg(test)]
 
 mod tests {
-use super::*;
+    use super::*;
 
-#[test]
-fn get_address_arb_works() {
-    let name = IsoName{raw_value: 0x8000000000000000};
-    let result = name.get_address_arb_capable();
+    #[test]
+    fn get_address_arb_works() {
+        let name = IsoName {
+            raw_value: 0x8000000000000000,
+        };
+        let result = name.get_address_arb_capable();
 
-    assert_eq!(result, 1);
-}
+        assert_eq!(result, 1);
+    }
 
-#[test]
-fn set_address_arb_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_address_arb_capable(1);
-    let result = name.get_address_arb_capable();
+    #[test]
+    fn set_address_arb_works() {
+        let mut name = IsoName::new(0x0);
 
-    assert_eq!(result, 1);
-}
+        name.set_address_arb_capable(1);
+        let result = name.get_address_arb_capable();
 
-#[test]
-fn get_industry_group_works() {
-    let name = IsoName{raw_value: 0x7000000000000000};
-    let result = name.get_industry_group();
+        assert_eq!(result, 1);
+    }
 
-    assert_eq!(result, 0x07);
-}
+    #[test]
+    fn get_industry_group_works() {
+        let name = IsoName {
+            raw_value: 0x7000000000000000,
+        };
+        let result = name.get_industry_group();
 
-#[test]
-fn set_industry_group_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_industry_group(0x7);
-    let result = name.get_industry_group();
+        assert_eq!(result, 0x07);
+    }
 
-    assert_eq!(result, 0x7);
-}
+    #[test]
+    fn set_industry_group_works() {
+        let mut name = IsoName::new(0x0);
 
-#[test]
-fn get_vehicle_system_instance_works() {
-    let name = IsoName{raw_value: 0x0F00000000000000};
-    let result = name.get_vehicle_system_instance();
+        name.set_industry_group(0x7);
+        let result = name.get_industry_group();
 
-    assert_eq!(result, 0x0F);
-}
+        assert_eq!(result, 0x7);
+    }
 
-#[test]
-fn set_vehicle_system_instance_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_vehicle_system_instance(0xF);
-    let result = name.get_vehicle_system_instance();
+    #[test]
+    fn get_vehicle_system_instance_works() {
+        let name = IsoName {
+            raw_value: 0x0F00000000000000,
+        };
+        let result = name.get_vehicle_system_instance();
 
-    assert_eq!(result, 0xF);
-}
+        assert_eq!(result, 0x0F);
+    }
 
-#[test]
-fn get_vehicle_system_works() {
-    let name = IsoName{raw_value: 0x00FE000000000000};
-    let result = name.get_vehicle_system();
+    #[test]
+    fn set_vehicle_system_instance_works() {
+        let mut name = IsoName::new(0x0);
 
-    assert_eq!(result, 0x7F);
-}
+        name.set_vehicle_system_instance(0xF);
+        let result = name.get_vehicle_system_instance();
 
-#[test]
-fn set_vehicle_system_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_vehicle_system(0x7F);
-    let result = name.get_vehicle_system();
+        assert_eq!(result, 0xF);
+    }
 
-    assert_eq!(result, 0x7F);
-}
+    #[test]
+    fn get_vehicle_system_works() {
+        let name = IsoName {
+            raw_value: 0x00FE000000000000,
+        };
+        let result = name.get_vehicle_system();
 
-#[test]
-fn get_function_code_works() {
-    let name = IsoName{raw_value: 0x0000FF0000000000};
-    let result = name.get_function_code();
+        assert_eq!(result, 0x7F);
+    }
 
-    assert_eq!(result, 0xFF);
-}
+    #[test]
+    fn set_vehicle_system_works() {
+        let mut name = IsoName::new(0x0);
 
-#[test]
-fn set_function_code_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_function_code(0xFF);
-    let result = name.get_function_code();
+        name.set_vehicle_system(0x7F);
+        let result = name.get_vehicle_system();
 
-    assert_eq!(result, 0xFF);
-}
+        assert_eq!(result, 0x7F);
+    }
 
-#[test]
-fn get_function_code_instance_works() {
-    let name = IsoName{raw_value: 0x000000F800000000};
-    let result = name.get_function_code_instance();
+    #[test]
+    fn get_function_code_works() {
+        let name = IsoName {
+            raw_value: 0x0000FF0000000000,
+        };
+        let result = name.get_function_code();
 
-    assert_eq!(result, 0x1F);
-}
+        assert_eq!(result, 0xFF);
+    }
 
-#[test]
-fn set_function_code_instance_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_function_code_instance(0x1F);
-    let result = name.get_function_code_instance();
+    #[test]
+    fn set_function_code_works() {
+        let mut name = IsoName::new(0x0);
 
-    assert_eq!(result, 0x1F);
-}
+        name.set_function_code(0xFF);
+        let result = name.get_function_code();
 
-#[test]
-fn get_ecu_instance_works() {
-    let name = IsoName{raw_value: 0x0000000700000000};
-    let result = name.get_ecu_instance();
+        assert_eq!(result, 0xFF);
+    }
 
-    assert_eq!(result, 0x7);
-}
+    #[test]
+    fn get_function_code_instance_works() {
+        let name = IsoName {
+            raw_value: 0x000000F800000000,
+        };
+        let result = name.get_function_code_instance();
 
-#[test]
-fn set_ecu_instance_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_ecu_instance(0x7);
-    let result = name.get_ecu_instance();
+        assert_eq!(result, 0x1F);
+    }
 
-    assert_eq!(result, 0x7);
-}
+    #[test]
+    fn set_function_code_instance_works() {
+        let mut name = IsoName::new(0x0);
 
-#[test]
-fn get_mc_code_works() {
-    let name = IsoName{raw_value: 0x00000000FFE00000};
-    let result = name.get_mc_code();
+        name.set_function_code_instance(0x1F);
+        let result = name.get_function_code_instance();
 
-    assert_eq!(result, 0x7FF);
-}
+        assert_eq!(result, 0x1F);
+    }
 
-#[test]
-fn set_mc_code_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_mc_code(0x7FF);
-    let result = name.get_mc_code();
+    #[test]
+    fn get_ecu_instance_works() {
+        let name = IsoName {
+            raw_value: 0x0000000700000000,
+        };
+        let result = name.get_ecu_instance();
 
-    assert_eq!(result, 0x7FF);
-}
+        assert_eq!(result, 0x7);
+    }
 
-#[test]
-fn get_serial_works() {
-    let name = IsoName{raw_value: 0x00000000001FFFFF};
-    let result = name.get_serial();
+    #[test]
+    fn set_ecu_instance_works() {
+        let mut name = IsoName::new(0x0);
 
-    assert_eq!(result, 0x1FFFFF);
-}
+        name.set_ecu_instance(0x7);
+        let result = name.get_ecu_instance();
 
-#[test]
-fn set_serial_works() {
-    let mut name = IsoName::new(0x0);
-    
-    name.set_serial(0x1FFFF);
-    let result = name.get_serial();
+        assert_eq!(result, 0x7);
+    }
 
-    assert_eq!(result, 0x1FFFF);
-}
+    #[test]
+    fn get_mc_code_works() {
+        let name = IsoName {
+            raw_value: 0x00000000FFE00000,
+        };
+        let result = name.get_mc_code();
 
+        assert_eq!(result, 0x7FF);
+    }
+
+    #[test]
+    fn set_mc_code_works() {
+        let mut name = IsoName::new(0x0);
+
+        name.set_mc_code(0x7FF);
+        let result = name.get_mc_code();
+
+        assert_eq!(result, 0x7FF);
+    }
+
+    #[test]
+    fn get_serial_works() {
+        let name = IsoName {
+            raw_value: 0x00000000001FFFFF,
+        };
+        let result = name.get_serial();
+
+        assert_eq!(result, 0x1FFFFF);
+    }
+
+    #[test]
+    fn set_serial_works() {
+        let mut name = IsoName::new(0x0);
+
+        name.set_serial(0x1FFFF);
+        let result = name.get_serial();
+
+        assert_eq!(result, 0x1FFFF);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,24 @@ use text_io::read;
 mod isoname;
 
 fn main() {
-    let args: Vec<String>= env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let mut valid_args = false; 
+    let mut valid_args = false;
     let mut return_code = 1;
-    if args.len() == 2
-    {
-        if args[1].contains("0x")
-        {
+    if args.len() == 2 {
+        if args[1].contains("0x") {
             valid_args = true;
             return_code = parse_isoname(args[1].clone());
-        }
-        else if args[1] == "--pack-name" || args[1] == "-p"
-        {
+        } else if args[1] == "--pack-name" || args[1] == "-p" {
             valid_args = true;
             pack_isoname();
-        }
-        else if args[1] == "--help" || args[1] == "-h"
-        {
+        } else if args[1] == "--help" || args[1] == "-h" {
             valid_args = true;
             print_help();
         }
     }
 
-    if !valid_args
-    {
+    if !valid_args {
         println!("Invalid Arguments!");
         print_help();
     }
@@ -37,75 +30,84 @@ fn main() {
     exit(return_code)
 }
 
-
-fn parse_isoname(full_isoname: String) -> i32
-{
-    if let Ok(isoname_integer) = u64::from_str_radix(full_isoname.trim_start_matches("0x"), 16)
-    {
+fn parse_isoname(full_isoname: String) -> i32 {
+    if let Ok(isoname_integer) = u64::from_str_radix(full_isoname.trim_start_matches("0x"), 16) {
         let name = isoname::IsoName::new(isoname_integer);
-        println!("Address Arbitration Capable: \t{}",  name.get_address_arb_capable());
-        println!("Industry Group: \t\t{}",  name.get_industry_group());
-        println!("Vehicle System Instance: \t{}",  name.get_vehicle_system_instance());
-        println!("Vehicle System (Device Class): \t{}",  name.get_vehicle_system());
-        println!("Function Code: \t\t\t{}",  name.get_function_code());
-        println!("Function Instance: \t\t{}",  name.get_function_code_instance());
-        println!("ECU Instance: \t\t\t{}",  name.get_ecu_instance());
-        println!("MC Code: \t\t\t{}",  name.get_mc_code());
+        println!(
+            "Address Arbitration Capable: \t{}",
+            name.get_address_arb_capable()
+        );
+        println!("Industry Group: \t\t{}", name.get_industry_group());
+        println!(
+            "Vehicle System Instance: \t{}",
+            name.get_vehicle_system_instance()
+        );
+        println!(
+            "Vehicle System (Device Class): \t{}",
+            name.get_vehicle_system()
+        );
+        println!("Function Code: \t\t\t{}", name.get_function_code());
+        println!(
+            "Function Instance: \t\t{}",
+            name.get_function_code_instance()
+        );
+        println!("ECU Instance: \t\t\t{}", name.get_ecu_instance());
+        println!("MC Code: \t\t\t{}", name.get_mc_code());
         println!("Serial: \t\t\t{}", name.get_serial());
-        
+
         return 0;
-    }
-    else{
+    } else {
         println!("Error parsing {full_isoname} as an isoname!");
         return 1;
     }
 }
 
-fn pack_isoname()
-{
-        let mut name = isoname::IsoName::new(0);
+fn pack_isoname() {
+    let mut name = isoname::IsoName::new(0);
 
-        print!("Address Arbitration Capable: ");
-        let address_arb_capable: u8 = read!();
-        name.set_address_arb_capable(address_arb_capable);
+    print!("Address Arbitration Capable: ");
+    let address_arb_capable: u8 = read!();
+    name.set_address_arb_capable(address_arb_capable);
 
-        print!("Industry Group: ");
-        let industry_group: u8 = read!();
-        name.set_industry_group(industry_group);
+    print!("Industry Group: ");
+    let industry_group: u8 = read!();
+    name.set_industry_group(industry_group);
 
-        print!("Vehicle System (Device Class) Instance: ");
-        let vehicle_system_instance: u8 = read!();
-        name.set_vehicle_system_instance(vehicle_system_instance);
+    print!("Vehicle System (Device Class) Instance: ");
+    let vehicle_system_instance: u8 = read!();
+    name.set_vehicle_system_instance(vehicle_system_instance);
 
-        print!("Vehicle System (Device Class): ");
-        let vehicle_system: u8 = read!();
-        name.set_vehicle_system(vehicle_system);
+    print!("Vehicle System (Device Class): ");
+    let vehicle_system: u8 = read!();
+    name.set_vehicle_system(vehicle_system);
 
-        print!("Function Code: ");
-        let function_code: u8 = read!();
-        name.set_function_code(function_code);
+    print!("Function Code: ");
+    let function_code: u8 = read!();
+    name.set_function_code(function_code);
 
-        print!("Function Code Instance: ");
-        let function_code_instance: u8 = read!();
-        name.set_function_code_instance(function_code_instance);
+    print!("Function Code Instance: ");
+    let function_code_instance: u8 = read!();
+    name.set_function_code_instance(function_code_instance);
 
-        print!("ECU Instance: ");
-        let ecu_instance: u8 = read!();
-        name.set_ecu_instance(ecu_instance);
+    print!("ECU Instance: ");
+    let ecu_instance: u8 = read!();
+    name.set_ecu_instance(ecu_instance);
 
-        print!("Manufacturer Code: ");
-        let mc_code: u16 = read!();
-        name.set_mc_code(mc_code);
-        
-        print!("Serial Number: ");
-        let serial: u32 = read!();
-        name.set_serial(serial);
+    print!("Manufacturer Code: ");
+    let mc_code: u16 = read!();
+    name.set_mc_code(mc_code);
 
-        println!("Isoname: 0x{:X}", name.get_raw_value());
+    print!("Serial Number: ");
+    let serial: u32 = read!();
+    name.set_serial(serial);
+
+    println!("Isoname: 0x{:X}", name.get_raw_value());
 }
 
-fn print_help(){
-    println!(r"Usage: isoname-parser [0x<64-bit hex number>] [options]
+fn print_help() {
+    println!(
+        r"Usage: isoname-parser [0x<64-bit hex number>] [options]
 Possible Options:
-    --pack-name|-p  Packs the ISOName based on the individual fields");
+    --pack-name|-p  Packs the ISOName based on the individual fields"
+    );
 }
